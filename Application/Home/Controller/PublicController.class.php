786e18a2e6c7420f6a0109108e52e2abfebedb2d
guangdian90 <315424751@qq.com>
2017-11-21 16:41:26 +0800

    update

--- Application/Home/Controller/PublicController.class.php
+++ Application/Home/Controller/PublicController.class.php
@@ -249,11 +249,23 @@ class PublicController extends BaseController {
             }
             //github 111111111
             //存到提现表
-            $recharge_total = $http_request_data['recharge_total'];//充值总额
+            $recharge_total = $http_request_data['recharge_total']/10;//充值总额
             $w_sql = 'select uid,sum(yingfu) as sum from withdrawals where uid = '.$uid.' and state = 3';
             $w_result_temp = D('withdrawals')->query($w_sql);
             $withdraw_total = $w_result_temp[0]['sum']?$w_result_temp[0]['sum']:0;//提现总额
-//            $diff = ($recharge_total == 0)?0:round(($recharge_total/10-$withdraw_total)/$recharge_total,2);//差额比例
+//            3000以下不检测
+//            3000以上充值不为0按百分比算  大于等于%50标红[(充值-提现)/充值]
+//            3000以上充值为0去掉  标红
+            $percent = ($recharge_total == 0)?-1:($recharge_total-$withdraw_total)/$recharge_total;//比率
+            $class = 0;//前端样式，正常0，标红1
+            if($withdraw_total >= 3000){
+                if($recharge_total == 0){
+                    $class = 1;
+                }elseif($percent*-1 >= 0.5){//负数越大，亏损越大
+                    $class = 1;
+                }
+            }
+
             $tData = array(
                 'uid'=>$uid,
                 'money'=>$http_request_data['money']/10,
@@ -271,6 +283,8 @@ class PublicController extends BaseController {
 //                'diff'=>$diff,//差额比例
                 'recharge_total'=>$recharge_total,//充值总额
                 'withdraw_total'=>$withdraw_total,//提现总额
+                'percent'=>$percent,//比例
+                'class'=>$class,
             );
             $res2 = $this->insAndUpdate('withdrawals','',$tData);
             if(!$res2){
